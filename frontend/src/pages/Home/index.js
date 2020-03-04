78794d83866ebec987217ecb083d4f4b3e7ed00d
Silvio Marques <silvio@impyco.com.br>
2020-03-04 12:16:21 -0300

    Implementation of connection with API

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,76 +1,48 @@
-import React from 'react';
-import { ProductList } from './styles';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
+import api from '../../services/api';
+import { formatPrice } from '../../utils/format';
 
-export default function Home() {
-    return (
-        <ProductList>
-            <li>
-                <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/26/HZM-1729-026/HZM-1729-026_zoom2.jpg?ims=326x" alt="Tênis Nike Revolution 5" />
-                <strong>Tênis Nike Revolution 5</strong>
-                <span>R$189,99</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADD TO CART</span>
-                </button>
-            </li>
-            <li>
-                <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/26/HZM-1729-026/HZM-1729-026_zoom2.jpg?ims=326x" alt="Tênis Nike Revolution 5" />
-                <strong>Tênis Nike Revolution 5</strong>
-                <span>R$189,99</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADD TO CART</span>
-                </button>
-            </li>
-            <li>
-                <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/26/HZM-1729-026/HZM-1729-026_zoom2.jpg?ims=326x" alt="Tênis Nike Revolution 5" />
-                <strong>Tênis Nike Revolution 5</strong>
-                <span>R$189,99</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADD TO CART</span>
-                </button>
-            </li>
-            <li>
-                <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/26/HZM-1729-026/HZM-1729-026_zoom2.jpg?ims=326x" alt="Tênis Nike Revolution 5" />
-                <strong>Tênis Nike Revolution 5</strong>
-                <span>R$189,99</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADD TO CART</span>
-                </button>
-            </li>
-            <li>
-                <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/26/HZM-1729-026/HZM-1729-026_zoom2.jpg?ims=326x" alt="Tênis Nike Revolution 5" />
-                <strong>Tênis Nike Revolution 5</strong>
-                <span>R$189,99</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADD TO CART</span>
-                </button>
-            </li>
-            <li>
-                <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/26/HZM-1729-026/HZM-1729-026_zoom2.jpg?ims=326x" alt="Tênis Nike Revolution 5" />
-                <strong>Tênis Nike Revolution 5</strong>
-                <span>R$189,99</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADD TO CART</span>
-                </button>
-            </li>
-        </ProductList>
-    );
+export default class Home extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            products: [],
+        }
+    }
+
+    async componentDidMount() {
+        const response = await api.get('products');
+
+        const data = response.data.map(product => ({
+            ...product,
+            priceFormatted: formatPrice(product.price).replace(/\xA0/, ""),
+        }));
+
+        this.setState({ products: data });
+    }
+
+    render() {
+        const { products } = this.state;
+
+        return (
+            <ProductList>
+                { products.map(product => (
+                    <li key={product.id}>
+                        <img src={product.image} alt={product.title} />
+                        <strong>{product.title}</strong>
+                        <span>{product.priceFormatted}</span>
+                        <button type="button">
+                            <div>
+                                <MdAddShoppingCart size={16} color="#fff" /> 3
+                            </div>
+                            <span>ADD TO CART</span>
+                        </button>
+                    </li>
+                )) }
+            </ProductList>
+        );
+    }
 }
